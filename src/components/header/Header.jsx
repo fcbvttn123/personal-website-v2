4292937c99ed6c34ce29c63d927347ff3edd4269
ngan <vutran@sheridancollege.ca>
2024-01-13 14:32:25 -0500

    Main Section is nearly Done
    Animation Left

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ export function Header({setOpenMobileMenu}) {
                     <p className="grid place-content-center">Front-end Developer</p>
                 </div>
                 <button className="w-8 h-8 hover:opacity-70 md:hidden" onClick={setOpenMobileMenu}>
-                    <img className="w-full h-full" src={menuIcon} alt="" srcset="" />
+                    <img className="w-full h-full" src={menuIcon} alt="" srcSet="" />
                 </button>
                 <div className="hidden md:flex items-center gap-x-4">
                     <button className="border-r border-black pr-4 hover:text-amber-500 transition-all duration-300">Resume</button>
--- src/components/main/MainSection.jsx
+++ src/components/main/MainSection.jsx
@@ -4,19 +4,19 @@ import avt from "./images/avt.jpeg"
 export function MainSection() {
     return (
         <main className="mt-4">
-            <div className="w-11/12 mx-auto max-w-screen-2xl md:flex">
-                <div>
-                    <h1 className="text-5xl font-semibold">Hello</h1>
-                    <h3 className="text-2xl font-semibold mt-4 mb-1">A bit about me</h3>
-                    <p>Passionate front-end developer with a keen eye for design and a love for crafting seamless user experiences. Proficient in HTML, Tailwind CSS, JavaScript and React JS, I specialize in turning ideas into visually stunning and interactive websites.</p>
+            <div className="w-11/12 mx-auto max-w-screen-2xl md:max-w-screen-xl md:flex flex-row-reverse items-center justify-center gap-x-10">
+                <div className="md:max-w-5xl flex-1">
+                    <h1 className="text-5xl sm:text-7xl md:text-8xl font-semibold">Hello</h1>
+                    <h3 className="text-2xl sm:text-3xl font-semibold mt-4 mb-1">A bit about me</h3>
+                    <p className="md:max-w-md">Passionate front-end developer with a keen eye for design and a love for crafting seamless user experiences. Proficient in HTML, Tailwind CSS, JavaScript and React JS, I specialize in turning ideas into visually stunning and interactive websites.</p>
                     <div className="flex items-center justify-between gap-x-4 mt-6 mx-auto md:mx-0 max-w-128">
                         <Circle backgroundColor="bg-amber-600">Resume</Circle>
                         <Circle backgroundColor="bg-red-500">Projects</Circle>
                         <Circle backgroundColor="bg-teal-400">Contact</Circle>
                     </div>
                 </div>
-                <div className="overflow-hidden rounded-full border border-zinc-200 mt-6 mx-auto max-w-96">
-                    <img className="w-full h-full" src={avt} alt="" srcset="" />
+                <div className="overflow-hidden rounded-full border border-zinc-200 mt-6 md:mt-0 mx-auto max-w-96 md:max-w-none flex-1">
+                    <img className="w-full h-full" src={avt} alt="" srcSet="" />
                 </div>
             </div>
         </main>
