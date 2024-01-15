35eebaf57c0a42c7a13e8a85c5b3fe4268fbbbe7
ngan <vutran@sheridancollege.ca>
2024-01-14 22:39:01 -0500

    Main Section is Done

--- src/components/main/Circle.jsx
+++ src/components/main/Circle.jsx
@@ -1,6 +1,6 @@
-export function Circle({children, backgroundColor}) {
+export function Circle({children, backgroundColor, moveUpAnimation}) {
     return (
-        <button className={`${backgroundColor} flex-1 rounded-full aspect-square border border-black text-xl font-semibold transition-all duration-500 hover:bg-white`}>
+        <button className={`${backgroundColor} ${moveUpAnimation} opacity-0 translate-y-24 flex-1 rounded-full aspect-square border border-black text-xl font-semibold transition-all duration-500 hover:bg-white`}>
             {children}
         </button>
     )
