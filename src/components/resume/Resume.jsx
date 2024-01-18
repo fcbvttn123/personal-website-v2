6eeea041b1fb2c8e0474037dc484f8c5e1b0f416
ngan <vutran@sheridancollege.ca>
2024-01-17 21:56:17 -0500

    Resume - Mobile Design Update
    Skill Section is Done

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,56 +1,123 @@
 export function Resume() {
     return (
-        <main className="mt-4">
-            <div className="w-11/12 mx-auto max-w-screen-2xl">
-                <h1 className="text-4xl font-extrabold mb-6">Resume</h1>
-                <div>
-                    <h3 className="text-2xl font-medium move-up-animation">Work Experience</h3>
-                    <div className="mt-4 opacity-0 move-up-animation-delay-80">
-                        <p className="font-thin">01/2023 - 04/2023</p>
-                        <p className="text-lg font-medium">Technical Analyst</p>
-                        <div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Effective communication in order to train on the use of tools and resolve technical issues</p>
-                            </div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Supporting creation of I&IT architecture artifacts based on Ontario Public Service</p>
-                            </div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Participating in IT security activities: security assessments, development and management of security deliverables</p>
-                            </div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Providing technical support in the maintenance and development of SharePoint repositories</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mt-4 opacity-0 move-up-animation-delay-100">
-                        <p className="font-thin">09/2023 - 01/2024</p>
-                        <p className="text-lg font-medium">Support Technologist</p>
-                        <div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Provide Classroom support to clients using the mobile labs, smart classrooms and Computer labs</p>
-                            </div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Resolve inquiries regarding desktop and laptop computers, operating systems</p>
-                            </div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Improved MacOS skills by using MacOS in Work</p>
-                            </div>
-                            <div className="flex items-start gap-x-2 mt-2">
-                                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
-                                <p>Successfully to stay for the par-time position after finishing the COOP Term</p>
-                            </div>
-                        </div>
-                    </div>
+      <main className="mt-4">
+        <div className="w-11/12 mx-auto max-w-screen-2xl">
+          <h1 className="text-4xl font-extrabold">Resume</h1>
+          <div className="mt-10">
+            <h3 className="text-2xl font-medium move-up-animation">Work Experience</h3>
+            <div className="mt-4 opacity-0 move-up-animation-delay-80">
+              <p className="font-thin">01/2023 - 04/2023</p>
+              <p className="text-lg font-medium">Technical Analyst</p>
+              <div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>
+                    Effective communication in order to train on the use of
+                    tools and resolve technical issues
+                  </p>
                 </div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>
+                    Supporting creation of I&IT architecture artifacts based on
+                    Ontario Public Service
+                  </p>
+                </div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>
+                    Participating in IT security activities: security
+                    assessments, development and management of security
+                    deliverables
+                  </p>
+                </div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>
+                    Providing technical support in the maintenance and
+                    development of SharePoint repositories
+                  </p>
+                </div>
+              </div>
             </div>
-        </main>
-    )
+            <div className="mt-4 opacity-0 move-up-animation-delay-100">
+              <p className="font-thin">09/2023 - 01/2024</p>
+              <p className="text-lg font-medium">Support Technologist</p>
+              <div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>
+                    Provide Classroom support to clients using the mobile labs,
+                    smart classrooms and Computer labs
+                  </p>
+                </div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>
+                    Resolve inquiries regarding desktop and laptop computers,
+                    operating systems
+                  </p>
+                </div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>Improved MacOS skills by using MacOS in Work</p>
+                </div>
+                <div className="flex items-start gap-x-2 mt-2">
+                  <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                  <p>
+                    Successfully to stay for the par-time position after
+                    finishing the COOP Term
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 opacity-0 move-up-animation-view">
+            <h3 className="text-2xl font-medium">Skills</h3>
+            <div className="flex items-start gap-x-2 mt-2">
+                <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+                <p>
+                    <span className="font-bold">Programming Languages: </span>
+                    HTML, CSS, JavaScript, Java
+                </p>
+            </div>
+            <div className="flex items-start gap-x-2 mt-2">
+              <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+              <p>
+                <span className="font-bold">Frameworks: </span> 
+                React JS, Tailwind CSS, Spring Boot, Angular
+              </p>
+            </div>
+            <div className="flex items-start gap-x-2 mt-2">
+              <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+              <p>
+                <span className="font-bold">Version Control: </span> 
+                Git/GitHub
+              </p>
+            </div>
+            <div className="flex items-start gap-x-2 mt-2">
+              <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+              <p>
+                <span className="font-bold">Databases: </span> 
+                Firebase, MySQL, H2
+              </p>
+            </div>
+            <div className="flex items-start gap-x-2 mt-2">
+              <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+              <p>
+                <span className="font-bold">Build Tools: </span> 
+                Vite, Maven, Parcel Bundler, NPM
+              </p>
+            </div>
+            <div className="flex items-start gap-x-2 mt-2">
+              <div className="w-2 h-2 rounded-full bg-black shrink-0 relative top-2"></div>
+              <p>
+                <span className="font-bold">Operating Systems: </span> 
+                Window, macOS, iOS
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
 }
