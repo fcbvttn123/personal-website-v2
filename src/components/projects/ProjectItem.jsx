afef94953de0328345ca547a02d7ae83369a1119
ngan <vutran@sheridancollege.ca>
2024-01-19 10:28:21 -0500

    Project is nearly Done. Animation and Desc left

--- src/components/projects/ProjectItem.jsx
+++ src/components/projects/ProjectItem.jsx
@@ -4,14 +4,29 @@ import { BulletPoint } from "../BulletPoint"
 export function ProjectItem({imgName, projectName, projectDesc, url, itemIndex}) {
     let animation = itemIndex > 1 ? "move-up-animation-view" : "move-up-animation"
     return (
-      <a href={url} className={`block mt-7 move-up-animation ${animation}`}>
-        <h1 className="text-3xl font-bold">{projectName}</h1>
-        <img className="my-5 object-cover" src={IMAGE_URL + imgName} />
-        <div>
-          {projectDesc.map((desc, index) => (
-            <BulletPoint key={index}>{desc}</BulletPoint>
-          ))}
-        </div>
-      </a>
+      <>
+        <a href={url} className={`block mt-7 move-up-animation lg:hidden ${animation}`}>
+          <h1 className="text-3xl font-bold">{projectName}</h1>
+          <img className="my-5 object-cover" src={IMAGE_URL + imgName} />
+          <div>
+            {projectDesc.map((desc, index) => (
+              <BulletPoint key={index}>{desc}</BulletPoint>
+            ))}
+          </div>
+        </a>
+        <a href={url} className={`hidden mt-16 move-up-animation lg:flex items-stretch gap-x-10 ${animation}`}>
+          <div className="flex-1">
+            <h1 className="text-3xl font-bold">{projectName}</h1>
+            <div>
+              {projectDesc.map((desc, index) => (
+                <BulletPoint key={index}>{desc}</BulletPoint>
+                ))}
+            </div>
+          </div>
+          <div className="flex-1">
+            <img className="my-0 object-cover w-full h-full" src={IMAGE_URL + imgName} />
+          </div>
+        </a>
+      </>
     );
 }
