f6300b2ab3bd7110d9b2f3667249912aa0172361
ngan <vutran@sheridancollege.ca>
2024-01-21 22:27:25 -0500

    Form Changes

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -39,7 +39,7 @@ export function ContactForm() {
         }
     }
     return (
-        <form className="mt-7 move-up-animation sm:mt-0 md:flex md:flex-1 md:flex-wrap" action="#">
+        <form onSubmit={formSubmission} className="mt-7 move-up-animation sm:mt-0 md:flex md:flex-1 md:flex-wrap" action="#">
             <div className="flex flex-col gap-y-1 mb-2 md:w-2/4 md:pr-2">
                 <label htmlFor="firstName" className="text-lg">First Name</label>
                 <input type="text" name="firstName" id="firstName" className="border hover:border-2 border-black bg-transparent h-12 pl-2" onChange={formDataChange} value={formData.firstName}/>
@@ -61,7 +61,7 @@ export function ContactForm() {
                     <label htmlFor="message" className="text-lg">Message</label>
                     <textarea name="message" id="message" cols="30" rows="5" className="border hover:border-2 border-black bg-transparent pl-2 pt-2" onChange={formDataChange} value={formData.message}></textarea>
                 </div>
-                <button onClick={formSubmission} className="p-3 mt-4 md:mt-0 scale-125 aspect-square rounded-full border border-black text-xl font-semibold bg-amber-600 transition-all duration-500 hover:bg-white">
+                <button className="p-3 mt-4 md:mt-0 scale-125 aspect-square rounded-full border border-black text-xl font-semibold bg-amber-600 transition-all duration-500 hover:bg-white">
                     Submit
                 </button>
             </div>
